47bbe961a90d7bb89a6afd456d3fe055a1f2de2d
DiegoHidenori <diegohidenori.ca@hotmail.com>
2024-08-21 18:38:46 -0400

    Fixed file structure

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Navbar() {
+    return (
+        <nav>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Projects</a>
+                <a href="#">My Life</a>
+                <a href="#">Goals</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
